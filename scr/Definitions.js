14aeeffd4677f03edd3bc6993fe8d89bef439ca7
Ewan Breakey <ebreakey@gmail.com>
2016-06-08 21:05:41 +1000

    Made Default Game Name 'New Game'

--- scr/Definitions.js
+++ scr/Definitions.js
@@ -1,7 +1,7 @@
 
 //define game settings
 function setDefinitions() {
-  window.game_title = function(){return "Stealth Game";} //SETS GAME NAME
+  window.game_title = function(){return "New Game";} //SETS GAME NAME
   window.game_background = function(){return '#FFFFFF';} //SETS CANVAS BACKGROUND
   window.game_wbackground = function(){return '#000000';} //SETS BODY BACKGROUND
   window.game_width = function(){return window.innerWidth;} //SET CANVAS WIDTH
