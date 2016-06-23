ba8ba73ae9842e9dfa87bbb82015aac5f13c74fe
Ewan Breakey <ebreakey@gmail.com>
2016-06-23 17:21:55 +1000

    Improve SL
    can scale and define frames

--- obj/Sprites.js
+++ obj/Sprites.js
@@ -1,7 +1,10 @@
 spriteData = {
   "example":{
-    "src":"gph/Example.png",
-    "width":32,
-    "height":32
+    "src": "gph/Example.png",
+    "width": 32,
+    "height": 32,
+    "scale": [1,1],
+    "frameSize": [32,32],
+    "frameOffset": [0,0]
   }
 }
