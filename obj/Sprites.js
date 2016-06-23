a5decc4f0d27bb342dc97077aa04d707eac9b505
Ewan Breakey <ebreakey@gmail.com>
2016-06-23 18:04:47 +1000

    SL now works
    and supports 'n'

--- obj/Sprites.js
+++ obj/Sprites.js
@@ -1,10 +1,18 @@
+/*
+
+Define images to be used by SL
+
+use 'n' to indicate natural size
+note: when using 'n' images may take up to 10 frames longer to load (usually 6)
+*/
+
 spriteData = {
   "example":{
     "src": "gph/Example.png",
-    "width": 32,
-    "height": 32,
+    "width": 'n',
+    "height": 'n',
     "scale": [1,1],
-    "frameSize": [32,32],
+    "frameSize": ['n','n'],
     "frameOffset": [0,0]
   }
 }
