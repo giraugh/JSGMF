3bbe3bcd676ab6cf1382a4522b6b92ac80495dc7
Ewan Breakey <ebreakey@gmail.com>
2016-06-24 18:18:34 +1000

    Improved Inputs system
    space works and can use arrow keys and caps lock or shift doesnt brake everything

--- obj/Inputs.js
+++ obj/Inputs.js
@@ -2,13 +2,14 @@
 This is used by the inputs library, define your games inputs here.
 
 NOTE: keys must be an array.
-NOTE: Keys may be an integer (keypress code, not keydown code) 
+NOTE: Keys may be an integer (keypress code, not keydown code)
+
+Below are some example inputs...
 */
 
 inputs = {
-   "jump": ["w"," "],
-   "right": ["d"],
-   "left": ["a"],
-   "up": ["w"],
-   "down": ["s"]
+   "up": ["w"," ","up"],
+   "right": ["d","right"],
+   "left": ["a","left"],
+   "down": ["s","down"]
 }
