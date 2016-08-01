ab9b2c785301f33c6a3573b27c2bbc5ff7aa9072
Ewan Breakey <ebreakey@gmail.com>
2016-08-01 21:34:59 +1000

    Update Libraries
    namely GUI

--- obj/Definitions.js
+++ obj/Definitions.js
@@ -1,17 +1,42 @@
+setDefinitions = function() {
+  window.game_title = function() {
+    return "New Game";
+  };
+  window.game_background = function() {
+    return '#FFFFFF';
+  };
+  window.game_wbackground = function() {
+    return '#FFFFF';
+  };
+  window.game_width = function() {
+    return 2000;
+  };
+  window.game_height = function() {
+    return 1000;
+  };
+  window.game_padding = function() {
+    return 0;
+  };
+  window.game_font = function() {
+    return "Lucida-Sans-Console";
+  };
+  window.game_scale = [1, 1];
 
-//define game settings
-function setDefinitions() {
-  window.game_title = function(){return "New Game";} //SETS GAME NAME
-  window.game_background = function(){return '#FFFFFF';} //SETS CANVAS BACKGROUND
-  window.game_wbackground = function(){return '#000000';} //SETS BODY BACKGROUND
-  window.game_width = function(){return window.innerWidth;} //SET CANVAS WIDTH
-  window.game_height = function(){return window.innerHeight;} //SETS CANVAS HEIGHT
-  window.game_padding = function(){return 0;} //SET BODY MARGIN
-}
+  window.get_left = function() {
+    return document.getElementById("canvas").getBoundingClientRect().left;
+  };
 
-//Keep canvas at fullscreen (because game is designed to be put in an iframe)
-function maintainCanvasFullscreen() {
-  window.game_width = function(){return window.innerWidth;} //SET CANVAS WIDTH
-  window.game_height = function(){return window.innerHeight;} //SETS CANVAS HEIGHT
+  return window.get_top = function() {
+    return document.getElementById("canvas").getBoundingClientRect().top;
+  };
+
+};
+
+maintainCanvasFullscreen = function() {
+  var scale, scale_x, scale_y;
+  scale_x = innerWidth / game_width();
+  scale_y = innerHeight / game_height();
+  scale = Math.min(Math.min(scale_x, scale_y), 1);
+  window.game_scale = [scale, scale];
   window.requestAnimationFrame(maintainCanvasFullscreen);
-}
+};
